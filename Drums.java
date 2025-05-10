8b0c7037a56fcb212734c37bba7f75e82d6f6d0d
João Torezan <jabtorezan@gmail.com>
2025-05-10 16:41:50 -0300

    Create Drums child class

---
+++ Drums.java
@@ -0,0 +1,30 @@
+public class Drums {
+   String shellMaterial;
+   int piecesCount;
+
+    public Drums() {
+        shellMaterial = "";
+        piecesCount = 0;
+    }
+
+    public Drums(String shellMaterial, int piecesCount) {
+        this.shellMaterial = shellMaterial;
+        this.piecesCount = piecesCount;
+    }
+   
+    public String getShellMaterial() {
+        return shellMaterial;
+    }
+
+    public int getPiecesCount() {
+        return piecesCount;
+    }
+
+    public void setShellMaterial(String shellMaterial) {
+        this.shellMaterial = shellMaterial;
+    }
+
+    public void setPiecesCount(int piecesCount) {
+        this.piecesCount = piecesCount;
+    }
+}
