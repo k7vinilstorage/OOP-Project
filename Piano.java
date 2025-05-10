211e8ca7d55da25030c73cc2dbde5027a4645ca0
João Torezan <jabtorezan@gmail.com>
2025-05-10 16:39:48 -0300

    Create child class Piano

---
+++ Piano.java
@@ -0,0 +1,30 @@
+public class Piano extends Instrument{
+    String bodyType;
+    int keyCount;
+
+    public Piano() {
+        bodyType = "";
+        keyCount = 0;
+    }
+
+    public Piano(String bodyType, int keyCount) {
+        this.bodyType = bodyType;
+        this.keyCount = keyCount;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getKeyCount() {
+        return keyCount;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public void setKeyCount(int keyCount) {
+        this.keyCount = keyCount;
+    }  
+}
