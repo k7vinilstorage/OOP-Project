97841df96c5bbe96e2e0c52a564a6e2d4a1c0838
João Torezan <jabtorezan@gmail.com>
2025-05-28 16:52:50 -0300

    joint storage classes

--- PeopleMenu.java
+++ PeopleMenu.java
@@ -1,3 +1,14 @@
 public class PeopleMenu {
-    
+    //get custumer
+    //get employee
+    //add employee
+    //add custumer
+    //update employee
+    //update custumer
+    //display employee
+    //display custumer
+    //del employee
+    //del custumer
+
+
 }
---
+++ Storage.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class Storage {
+    private ArrayList<Guitar> guitarStock;
+    private ArrayList<Piano> pianoStock;
+    private ArrayList<Drums> drumsStock;
+    private ArrayList<Employee> employees;
+    private ArrayList<Customer> customers;
+    private ArrayList<Sale> sales;
+    private ArrayList<Acquisition> acquisitions;
+
+    public Storage() {
+    }
+
+    public Storage(ArrayList<Guitar> guitarStock, ArrayList<Piano> pianoStock, ArrayList<Drums> drumsStock, ArrayList<Employee> employees, ArrayList<Customer> customers, ArrayList<Sale> sales, ArrayList<Acquisition> acquisitions) {
+        this.guitarStock = guitarStock;
+        this.pianoStock = pianoStock;
+        this.drumsStock = drumsStock;
+        this.employees = employees;
+        this.customers = customers;
+        this.sales = sales;
+        this.acquisitions = acquisitions;
+    }
+
+    public ArrayList<Guitar> getGuitarStock() {
+        return guitarStock;
+    }
+
+    public ArrayList<Piano> getPianoStock() {
+        return pianoStock;
+    }
+    public ArrayList<Drums> getDrumsStock() {
+        return drumsStock;
+    }
+
+    public void setGuitarStock(ArrayList<Guitar> guitarStock) {
+        this.guitarStock = guitarStock;
+    }
+
+    public void setPianoStock(ArrayList<Piano> pianoStock) {
+        this.pianoStock = pianoStock;
+    }
+
+    public void setDrumsStock(ArrayList<Drums> drumsStock) {
+        this.drumsStock = drumsStock;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<Sale> getSales() {
+        return sales;
+    }
+
+    public ArrayList<Acquisition> getAcquisitions() {
+        return acquisitions;
+    }
+
+    public void setSales(ArrayList<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public void setAcquisitions(ArrayList<Acquisition> acquisitions) {
+        this.acquisitions = acquisitions;
+    }
+}
